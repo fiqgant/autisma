1881245c092786f54ffbcce16b6b45f5db3a216c
Taufiqurrahman <fiqgant@gmail.com>
2024-11-06 14:52:29 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a24df330347637883740ebb24bf52332.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0fe5d537307e59722b58ffe38c68ff",
+    "url": "/autisma/index.html"
+  },
+  {
+    "revision": "5a61126e06edfc895c77",
+    "url": "/autisma/static/css/main.53eff1e4.chunk.css"
+  },
+  {
+    "revision": "824d5d48d17251f4c954",
+    "url": "/autisma/static/js/2.d1d932cd.chunk.js"
+  },
+  {
+    "revision": "5a61126e06edfc895c77",
+    "url": "/autisma/static/js/main.e230ecc0.chunk.js"
+  },
+  {
+    "revision": "9ea99b033d2ef4878833",
+    "url": "/autisma/static/js/runtime-main.923f6276.js"
+  },
+  {
+    "revision": "316ca27912264dadb88c39d8c81436a5",
+    "url": "/autisma/static/media/Airport.316ca279.png"
+  },
+  {
+    "revision": "d4916bdda20d36936376a2fd7a5cd22d",
+    "url": "/autisma/static/media/Airport.d4916bdd.svg"
+  },
+  {
+    "revision": "55ee47acb616455aa38e7d76aa3971d6",
+    "url": "/autisma/static/media/Airport_icon.55ee47ac.svg"
+  },
+  {
+    "revision": "a2b8944f6a362f765a010ca116ef137e",
+    "url": "/autisma/static/media/Autisma.a2b8944f.png"
+  },
+  {
+    "revision": "40754880ca0883817310b6d9b1c87131",
+    "url": "/autisma/static/media/AutismaHelpFinal.40754880.mp4"
+  },
+  {
+    "revision": "82736d66c60d2a0cb5fda6fd27fb0ec9",
+    "url": "/autisma/static/media/Background_Home.82736d66.svg"
+  },
+  {
+    "revision": "c2ffe075932493fb94355859ddb0b111",
+    "url": "/autisma/static/media/Blackboard.c2ffe075.svg"
+  },
+  {
+    "revision": "d31e3373609379367d7c5dc0c36d9443",
+    "url": "/autisma/static/media/Blackboard.d31e3373.png"
+  },
+  {
+    "revision": "688a4e29527fe2fdbfe06ac59e5af7f0",
+    "url": "/autisma/static/media/Boys_ball.688a4e29.svg"
+  },
+  {
+    "revision": "d3646b91e0146f942ebf371a5d2bebe3",
+    "url": "/autisma/static/media/Boys_ball.d3646b91.png"
+  },
+  {
+    "revision": "2b974a9780051d91d20a2efbccc170da",
+    "url": "/autisma/static/media/Bullying.2b974a97.svg"
+  },
+  {
+    "revision": "c6970f088e36101422121f0a3fab0979",
+    "url": "/autisma/static/media/Bullying.c6970f08.png"
+  },
+  {
+    "revision": "3f373dc6fd8f378473d7c1c83ce7bf01",
+    "url": "/autisma/static/media/Classroom.3f373dc6.svg"
+  },
+  {
+    "revision": "67d4f2b8f73c73e24d839450760f9ff3",
+    "url": "/autisma/static/media/Classroom.67d4f2b8.png"
+  },
+  {
+    "revision": "edf31e0649f5f3744fdba271b3862817",
+    "url": "/autisma/static/media/Fight_ball.edf31e06.png"
+  },
+  {
+    "revision": "fd3f51b2f990b8a45a0be0d74d4db94d",
+    "url": "/autisma/static/media/Fight_ball.fd3f51b2.svg"
+  },
+  {
+    "revision": "9bd1dce77a18d47677952f82cd661f2c",
+    "url": "/autisma/static/media/Hippo.9bd1dce7.svg"
+  },
+  {
+    "revision": "778c861333283406728c73c3fd491276",
+    "url": "/autisma/static/media/Hugging.778c8613.svg"
+  },
+  {
+    "revision": "ab6cbe5b5b155c10503e198207d28780",
+    "url": "/autisma/static/media/Hugging.ab6cbe5b.png"
+  },
+  {
+    "revision": "e8bf0a249802c5676ebea2adc40c2585",
+    "url": "/autisma/static/media/Isolation.e8bf0a24.svg"
+  },
+  {
+    "revision": "eb47b67b7ce9f5500a98e919cab82236",
+    "url": "/autisma/static/media/Isolation.eb47b67b.png"
+  },
+  {
+    "revision": "06dadf92ed27d7de0b33d0224d33c6aa",
+    "url": "/autisma/static/media/Playground.06dadf92.svg"
+  },
+  {
+    "revision": "5a03a2ebf5ffcdc341ebe7383197e93f",
+    "url": "/autisma/static/media/Playground.5a03a2eb.png"
+  },
+  {
+    "revision": "3223a7cb9029a48359b5e6434207fe4c",
+    "url": "/autisma/static/media/Playground_icon.3223a7cb.svg"
+  },
+  {
+    "revision": "de1e370c7c86a534d5eb66451df0af50",
+    "url": "/autisma/static/media/Running.de1e370c.svg"
+  },
+  {
+    "revision": "ea215c8da166deb81a32c85336e7c523",
+    "url": "/autisma/static/media/Running.ea215c8d.png"
+  },
+  {
+    "revision": "7f354d0eb8a09d9692f21338b5b6dd8f",
+    "url": "/autisma/static/media/School.7f354d0e.png"
+  },
+  {
+    "revision": "9078e4f37d98f6381e9c8acbb56f5f26",
+    "url": "/autisma/static/media/School.9078e4f3.svg"
+  },
+  {
+    "revision": "ee4f6caf16db0b890d2ed70c56c31aac",
+    "url": "/autisma/static/media/School_icon.ee4f6caf.svg"
+  },
+  {
+    "revision": "cef28f75237d4872ae2ee14fc6ad85bb",
+    "url": "/autisma/static/media/Slide.cef28f75.png"
+  },
+  {
+    "revision": "ec84705901379582e0236c3e5e8c7b2c",
+    "url": "/autisma/static/media/Slide.ec847059.svg"
+  },
+  {
+    "revision": "8f5e66be921ee511c1e91cad01f8be85",
+    "url": "/autisma/static/media/Slide_happy.8f5e66be.svg"
+  },
+  {
+    "revision": "a69fec32ea17a29e2658a37516d09593",
+    "url": "/autisma/static/media/Slide_happy.a69fec32.png"
+  },
+  {
+    "revision": "10499885060a829d9cb0b5807ec517cb",
+    "url": "/autisma/static/media/Talking.10499885.svg"
+  },
+  {
+    "revision": "567facd9358b162887e9fa2acd1ba0d4",
+    "url": "/autisma/static/media/Talking.567facd9.png"
+  },
+  {
+    "revision": "43669b2d9e87d587755e38346ba45006",
+    "url": "/autisma/static/media/Town.43669b2d.svg"
+  },
+  {
+    "revision": "338595cb7f848ce2471430eca7d96d00",
+    "url": "/autisma/static/media/lock.338595cb.svg"
+  },
+  {
+    "revision": "c652debec851e0b0cc9f3df71440cddd",
+    "url": "/autisma/static/media/map.c652debe.png"
+  }
+]);
